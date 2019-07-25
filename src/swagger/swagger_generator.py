a6c90ce9850b0614b7446e3e29328d1ec4a174f2
Keiran Price <keiran.price@stfc.ac.uk>
2019-07-25 12:05:16 +0100

    #15: Create method for converting from PascalCase

--- src/swagger/swagger_generator.py
+++ src/swagger/swagger_generator.py
@@ -1,9 +1,23 @@
 import os
+import re
+
+
 class SwaggerGenerator(object):
     FILE_PATH = os.getcwd() + "\\swagger\\openapi.yaml"
 
     def __init__(self):
         self.endpoints = []
+
+    @staticmethod
+    def pascal_to_normal(input):
+        """
+        Converts PascalCase to words seperated by spaces. All lowercase
+
+        :param input: The PascalCase input to be converted
+        :return: The converted string
+        """
+        words = re.findall(r"[A-Z]?[a-z]+|[A-Z]{2,}(?=[A-Z][a-z]|\d|\W|$)|\d+", input)
+        return " ".join(map(str.lower, words))
 
     def resource_wrapper(self):
         """
