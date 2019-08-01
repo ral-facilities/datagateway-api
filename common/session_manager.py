31062038b4fd368a743a0f198487d22dd0a88828
Keiran Price <keiran.price@stfc.ac.uk>
2019-08-01 08:13:05 +0100

    Move the session manager and use scoped_session

---
+++ common/session_manager.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from common.constants import Constants
+
+
+class SessionManager(object):
+    def __init__(self):
+        self.engine = create_engine(Constants.DATABASE_URL)
+        self.session_factory = sessionmaker(bind=self.engine)
+        self.Session = scoped_session(self.session_factory)
+
+    def get_icat_db_session(self):
+        """
+        Gets a new session in the scoped_session collection
+        :return: A new session
+        """
+
+        return self.Session()
+
+
+session_manager = SessionManager()
