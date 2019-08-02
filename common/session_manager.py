beb9df8d9703029a1cc13ff67cb39aba0bb907fb
Keiran Price <keiran.price@stfc.ac.uk>
2019-08-02 08:37:12 +0100

    Add pool_size and only create 1 engine/factory

--- common/session_manager.py
+++ common/session_manager.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.pool import QueuePool
 
 from common.constants import Constants
 
+engine = create_engine(Constants.DATABASE_URL, poolclass=QueuePool, pool_size=100, max_overflow=0)
+session_factory = sessionmaker(engine)
+
 
 class SessionManager(object):
+
     def __init__(self):
-        self.engine = create_engine(Constants.DATABASE_URL, pool_size=1000)
-        self.session_factory = sessionmaker(bind=self.engine)
-        self.Session = scoped_session(self.session_factory)
+        self.Session = scoped_session(session_factory)
 
     def get_icat_db_session(self):
         """
