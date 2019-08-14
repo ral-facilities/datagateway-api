7f13768e0b8a7bd02e953f85f41a4cb01b978cb6
Keiran Price <keiran.price@stfc.ac.uk>
2019-08-14 13:52:42 +0100

    #34: Add table specific resource classes

---
+++ src/resources/table_endpoints/table_endpoints.py
@@ -0,0 +1,26 @@
+from flask_restful import Resource
+
+
+class UsersInvestigations(Resource):
+    pass
+
+
+class UsersInvestigationsCount(Resource):
+    pass
+
+
+class InstrumentsFacilityCycles(Resource):
+    pass
+
+
+class InstrumentsFacilityCyclesCount(Resource):
+    pass
+
+
+class InstrumentsFacilityCyclesInvestigations(Resource):
+    pass
+
+
+class InstrumentsFacilityCyclesInvestigationsCount(Resource):
+    pass
+
