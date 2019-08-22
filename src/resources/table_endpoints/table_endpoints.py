ccfb01e85cbc1766a73358e878a6f6194c16d567
Keiran Price <keiran.price@stfc.ac.uk>
2019-08-22 08:05:51 +0100

    #34: Add ISIS User Investigations endpoint

--- src/resources/table_endpoints/table_endpoints.py
+++ src/resources/table_endpoints/table_endpoints.py
@@ -1,8 +1,14 @@
 from flask_restful import Resource
 
+from common.database_helpers import get_investigations_for_user
+from common.helpers import requires_session_id, queries_records, get_filters_from_query_string
+
 
 class UsersInvestigations(Resource):
-    pass
+    @requires_session_id
+    @queries_records
+    def get(self, id):
+        return get_investigations_for_user(id, get_filters_from_query_string()), 200
 
 
 class UsersInvestigationsCount(Resource):
