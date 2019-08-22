de749844914c8dedd9c9b1b7c7ad21498a168e69
Keiran Price <keiran.price@stfc.ac.uk>
2019-08-22 09:29:09 +0100

    #34: Update resource class for count

--- src/resources/table_endpoints/table_endpoints.py
+++ src/resources/table_endpoints/table_endpoints.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 
 from common.database_helpers import get_investigations_for_user, get_investigations_for_user_count, \
-    get_facility_cycles_for_instrument
+    get_facility_cycles_for_instrument, get_facility_cycles_for_instrument_count
 from common.helpers import requires_session_id, queries_records, get_filters_from_query_string
 
 
@@ -27,7 +27,10 @@ class InstrumentsFacilityCycles(Resource):
 
 
 class InstrumentsFacilityCyclesCount(Resource):
-    pass
+    @requires_session_id
+    @queries_records
+    def get(self, id):
+        return get_facility_cycles_for_instrument_count(id), 200
 
 
 class InstrumentsFacilityCyclesInvestigations(Resource):
