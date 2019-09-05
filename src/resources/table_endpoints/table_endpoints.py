1f6a915f5902183a0aaa6aa8108b966cba9b150c
Keiran Price <keiran.price@stfc.ac.uk>
2019-09-05 07:37:43 +0100

    #34: Add missing filter params

--- src/resources/table_endpoints/table_endpoints.py
+++ src/resources/table_endpoints/table_endpoints.py
@@ -31,7 +31,7 @@ class InstrumentsFacilityCyclesCount(Resource):
     @requires_session_id
     @queries_records
     def get(self, id):
-        return get_facility_cycles_for_instrument_count(id), 200
+        return get_facility_cycles_for_instrument_count(id, get_filters_from_query_string()), 200
 
 
 class InstrumentsFacilityCyclesInvestigations(Resource):
@@ -46,4 +46,5 @@ class InstrumentsFacilityCyclesInvestigationsCount(Resource):
     @requires_session_id
     @queries_records
     def get(self, instrument_id, cycle_id):
-        return get_investigations_for_instrument_in_facility_cycle_count(instrument_id, cycle_id), 200
+        return get_investigations_for_instrument_in_facility_cycle_count(instrument_id, cycle_id,
+                                                                         get_filters_from_query_string()), 200
