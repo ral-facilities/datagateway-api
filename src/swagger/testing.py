1a610a2deb61aaf2090a617ced8521ee3e4b86a2
Keiran Price <keiran.price@stfc.ac.uk>
2019-09-30 10:48:28 +0100

    Revert "Revert commit"
    This reverts commit bfd0e2901a15031520763074fc7d912ff324f0ac.

--- src/swagger/testing.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-AUTHORIZATION_HEADER = {"Authorization": "Bearer 25fa63a6-85d2-11e9-8530-dcbd99df7bb4"}
-
-print(requests.get("http://localhost:5000/datafiles/1",headers=AUTHORIZATION_HEADER).json())
