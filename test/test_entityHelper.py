22b1838a86cd2b9c5d4a9c84527bc723404239c5
Keiran Price <keiran.price@stfc.ac.uk>
2019-10-23 10:02:15 +0100

    #93: Test dictionary include

--- test/test_entityHelper.py
+++ test/test_entityHelper.py
@@ -76,7 +76,7 @@ class TestEntityHelper(TestCase):
                 "COMPLETE": None,
                 "DESCRIPTION": None,
                 "DOI": None,
-                "END_DATE":None,
+                "END_DATE": None,
                 "LOCATION": None,
                 "NAME": None,
                 "STARTDATE": None,
@@ -85,6 +85,58 @@ class TestEntityHelper(TestCase):
             }
         }
         self.assertEqual(expected_dict, self.datafile.to_nested_dict("DATASET"))
+        expected_dict = {
+            "ID": 1,
+            "LOCATION": "test location",
+            "NAME": "test name",
+            "MOD_TIME": str(datetime.datetime(2000, 1, 1)),
+            "CHECKSUM": "test checksum",
+            "FILESIZE": 64,
+            "DATAFILEMODTIME": str(datetime.datetime(2000, 1, 1)),
+            "DATAFILECREATETIME": str(datetime.datetime(2000, 1, 1)),
+            "DATASET_ID": 1,
+            "DOI": "test doi",
+            "DESCRIPTION": "test description",
+            "CREATE_ID": "test create id",
+            "MOD_ID": "test mod id",
+            "DATAFILEFORMAT_ID": 1,
+            "CREATE_TIME": str(datetime.datetime(2000, 1, 1)),
+            "DATASET": {
+                "ID": None,
+                "CREATE_TIME": None,
+                "MOD_TIME": None,
+                "CREATE_ID": None,
+                "MOD_ID": None,
+                "INVESTIGATION_ID": None,
+                "COMPLETE": None,
+                "DESCRIPTION": None,
+                "DOI": None,
+                "END_DATE": None,
+                "LOCATION": None,
+                "NAME": None,
+                "STARTDATE": None,
+                "SAMPLE_ID": None,
+                "TYPE_ID": None,
+                "INVESTIGATION": {
+                    "ID": None,
+                    "CREATE_ID": None,
+                    "CREATE_TIME": None,
+                    "DOI": None,
+                    "ENDDATE": None,
+                    "MOD_ID": None,
+                    "MOD_TIME": None,
+                    "NAME": None,
+                    "RELEASEDATE": None,
+                    "STARTDATE": None,
+                    "SUMMARY": None,
+                    "TITLE": None,
+                    "VISIT_ID": None,
+                    "FACILITY_ID": None,
+                    "TYPE_ID": None
+                }
+            }
+        }
+        self.assertEqual(expected_dict, self.datafile.to_nested_dict({"DATASET": "INVESTIGATION"}))
 
     def test_get_related_entity(self):
         self.assertEqual(self.dataset, self.datafile.get_related_entity("DATASET"))
