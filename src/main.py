09740125f78c5782dedde36a8dc1d50e6540a4fe
Keiran Price <keiran.price@stfc.ac.uk>
2019-10-23 09:36:15 +0100

    #102: Add bracket

--- src/main.py
+++ src/main.py
@@ -42,4 +42,4 @@ api.add_resource(InstrumentsFacilityCyclesInvestigationsCount,
                  "/instruments/<int:instrument_id>/facilitycycles/<int:cycle_id>/investigations/count")
 
 if __name__ == "__main__":
-    app.run(host=config.get_host(), port=config.get_port(), debug=config.is_debug_mode()
+    app.run(host=config.get_host(), port=config.get_port(), debug=config.is_debug_mode())
