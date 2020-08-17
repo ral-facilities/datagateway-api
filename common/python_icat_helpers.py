853b0a1a98a686fe21bae2b4291d361b8ef97430
Matthew Richards <matthew.richards@stfc.ac.uk>
2020-08-17 14:41:24 +0000

    #135: Make client objects bound to an endpoint, not a backend
    - This will prevent the same client object being used across different endpoints, which could be a security concern. This means that for each request, the client object is built with the session ID assigned each time, so each request checks that the user has a valid, active session ID

--- common/python_icat_helpers.py
+++ common/python_icat_helpers.py
@@ -3,10 +3,14 @@ import logging
 from datetime import datetime, timedelta
 
 
+import icat.client
 from icat.exception import ICATSessionError
+
 from common.exceptions import AuthenticationError
+from common.config import config
 
 log = logging.getLogger()
+
 
 def requires_session_id(method):
     """
@@ -29,8 +33,10 @@ def requires_session_id(method):
     @wraps(method)
     def wrapper_requires_session(*args, **kwargs):
         try:
-            client = args[0].client
+            client = create_client()
             client.sessionId = args[1]
+            # Client object put into kwargs so it can be accessed by backend functions
+            kwargs["client"] = client
 
             # Find out if session has expired
             session_time = client.getRemainingMinutes()
@@ -58,14 +64,25 @@ def queries_records(method):
     return wrapper_gets_records
 
 
+def create_client():
+    client = icat.client.Client(
+        config.get_icat_url(), checkCert=config.get_icat_check_cert()
+    )
+    return client
+
+
 def get_session_details_helper(client):
-    # Remove rounding 
+    # Remove rounding
     session_time_remaining = client.getRemainingMinutes()
     session_expiry_time = datetime.now() + timedelta(minutes=session_time_remaining)
 
     username = client.getUserName()
 
-    return {"ID": client.sessionId, "EXPIREDATETIME": str(session_expiry_time), "USERNAME": username}
+    return {
+        "ID": client.sessionId,
+        "EXPIREDATETIME": str(session_expiry_time),
+        "USERNAME": username,
+    }
 
 
 def logout_icat_client(client):
