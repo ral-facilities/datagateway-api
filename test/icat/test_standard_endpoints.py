bead8cfd5a10d2214601a57cb25ae7f8fc983c45
Matthew Richards <matthew.richards@stfc.ac.uk>
2020-11-25 09:18:02 +0000

    #150: Add more invalid tests for update endpoint

--- test/icat/test_standard_endpoints.py
+++ test/icat/test_standard_endpoints.py
@@ -155,23 +155,46 @@ class TestStandardEndpoints:
 
         assert response_json == single_investigation_test_data
 
-    def test_invalid_update_data(
+    def test_invalid_missing_update_data(
         self, flask_test_app, valid_credentials_header, single_investigation_test_data,
     ):
         """There should be an ID in the request body to know which entity to update"""
 
-        expected_doi = "Test Data Identifier"
-        expected_summary = "Test Summary"
-
         update_data_json = {
-            "doi": expected_doi,
-            "summary": expected_summary,
+            "doi": "Test Data Identifier",
+            "summary": "Test Summary",
         }
-        single_investigation_test_data[0]["doi"] = expected_doi
-        single_investigation_test_data[0]["summary"] = expected_summary
 
         test_response = flask_test_app.patch(
             "/investigations", headers=valid_credentials_header, json=update_data_json,
+        )
+
+        assert test_response.status_code == 400
+
+    @pytest.mark.parametrize(
+        "update_key, update_value",
+        [
+            pytest.param("invalidAttr", "Some Value", id="invalid attribute"),
+            pytest.param("modId", "simple/root", id="meta attribute update"),
+        ],
+    )
+    def test_invalid_attribute_update(
+        self,
+        flask_test_app,
+        valid_credentials_header,
+        single_investigation_test_data,
+        update_key,
+        update_value,
+    ):
+        invalid_update_data_json = {
+            "id": single_investigation_test_data[0]["id"],
+            update_key: update_value,
+        }
+
+        test_response = flask_test_app.patch(
+            "/investigations",
+            headers=valid_credentials_header,
+            json=invalid_update_data_json,
         )
 
         assert test_response.status_code == 400
