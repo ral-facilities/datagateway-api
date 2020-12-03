bcea7ec46e0c1819d8a7ae1d55e2f1d0b78fc5d7
Matthew Richards <matthew.richards@stfc.ac.uk>
2020-12-03 13:03:20 +0000

    #150: Fix FlaskAppTest based on changes to main.py
    - This is a temporary fix before rewriting the original tests in pytest

--- test/test_base.py
+++ test/test_base.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
-from datagateway_api.src.main import app
+from flask import Flask
+
+from datagateway_api.src.main import create_api_endpoints, create_app_infrastructure
 
 
 class FlaskAppTest(TestCase):
@@ -9,5 +11,11 @@ class FlaskAppTest(TestCase):
     """
 
     def setUp(self):
+        app = Flask(__name__)
         app.config["TESTING"] = True
+        app.config["TESTING"] = True
+        app.config["TEST_BACKEND"] = "db"
+
+        api, spec = create_app_infrastructure(app)
+        create_api_endpoints(app, api, spec)
         self.app = app.test_client()
