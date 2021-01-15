0a50a69e63ab66eeb7bd450ad9ae76ce3374c5e6
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-01-15 14:14:14 +0000

    #163: Add more versions of Python to build against

--- noxfile.py
+++ noxfile.py
@@ -82,7 +82,7 @@ def safety(session):
             session.log("Error: The temporary requirements file could not be closed")
 
 
-@nox.session(python=["3.6", "3.7", "3.8"], reuse_venv=True)
+@nox.session(python=["3.4", "3.5", "3.6", "3.7", "3.8", "3.9", "3.10"], reuse_venv=True)
 def tests(session):
     args = session.posargs
     session.run("poetry", "install", external=True)
