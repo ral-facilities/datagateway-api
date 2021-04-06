4ed8061affba0cb1727bc2cbbd32ecb850d5a587
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-04-06 12:57:52 +0000

    #209: Remove irrelevant TODO

--- datagateway_api/common/icat/helpers.py
+++ datagateway_api/common/icat/helpers.py
@@ -85,8 +85,6 @@ def get_cached_client(session_id, client_pool):
     session ID is flushed and the client is returned to the pool. More details about
     client handling can be found in the README
 
-    TODO - Add details of client handling in the README
-
     :param session_id: The user's session ID
     :type session_id: :class:`str`
     :param client_pool: Client object pool used to fetch an unused client
