bdc72fbae49ef9f2622c0433444de54c8099367a
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-04-06 12:10:23 +0000

    #209: Increase test coverage for extended cache

--- test/icat/test_lru_cache.py
+++ test/icat/test_lru_cache.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock
 from cachetools import cached
 
 from datagateway_api.common.config import config
+from datagateway_api.common.icat.icat_client_pool import create_client_pool
 from datagateway_api.common.icat.lru_cache import ExtendedLRUCache
 
 
@@ -13,14 +14,15 @@ class TestLRUCache:
 
     def test_valid_popitem(self, icat_client):
         test_cache = ExtendedLRUCache()
+        test_pool = create_client_pool()
 
         test_cache.popitem = MagicMock(side_effect=test_cache.popitem)
 
         @cached(cache=test_cache)
-        def get_cached_client(cache_number):
+        def get_cached_client(cache_number, client_pool):
             return icat_client
 
         for cache_number in range(config.get_client_cache_size() + 1):
-            get_cached_client(cache_number)
+            get_cached_client(cache_number, test_pool)
 
         assert test_cache.popitem.called
