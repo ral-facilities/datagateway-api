ede08f2050dce3e8f1637b0d35a96285085ebbf2
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-04-12 19:23:59 +0000

    #141: Add test cases for distinct attribute mapping

--- test/icat/test_query.py
+++ test/icat/test_query.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from icat.entity import Entity
 import pytest
@@ -163,6 +163,22 @@ class TestICATQuery:
                 id="Single attribute",
             ),
             pytest.param(
+                ["startDate"],
+                [
+                    datetime(
+                        year=2020,
+                        month=1,
+                        day=4,
+                        hour=1,
+                        minute=1,
+                        second=1,
+                        tzinfo=timezone.utc,
+                    )
+                ],
+                {"startDate": "2020-01-04 01:01:01+00:00"},
+                id="Single date attribute",
+            ),
+            pytest.param(
                 ["summary", "title"],
                 ["Summary 1", "Title 1"],
                 {"summary": "Summary 1", "title": "Title 1"},
@@ -173,6 +189,17 @@ class TestICATQuery:
                 ["Summary 1", "PI"],
                 {"summary": "Summary 1", "investigationUsers": {"role": "PI"}},
                 id="Multiple attributes with related attribute",
+            ),
+            pytest.param(
+                ["summary", "investigationUsers.investigation.name"],
+                ["Summary 1", "Investigation Name 1"],
+                {
+                    "summary": "Summary 1",
+                    "investigationUsers": {
+                        "investigation": {"name": "Investigation Name 1"}
+                    },
+                },
+                id="Multiple attributes with 2-level nested related attribute",
             ),
         ],
     )
