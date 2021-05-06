1b7f1325c433cf0917bdbc8e557468a1f110296e
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-05-06 17:29:38 +0000

    #223: Allow DatabaseDistinctFilter to recognise related entity inputs
    - This takes existing code from the DatabaseWhereFilter and makes it generic. Future commits will get the WhereFilter to also use this generic version in the same way as the DistinctFilter

--- datagateway_api/common/database/filters.py
+++ datagateway_api/common/database/filters.py
@@ -18,6 +18,85 @@ from datagateway_api.common.helpers import get_entity_object_from_name
 log = logging.getLogger()
 
 
+class DatabaseFilterUtilities:
+    """
+    Class containing utility functions used in the WhereFilter and DistinctFilter
+
+    In this class, the terminology of 'included entities' has been made more generic to
+    'related entities'. When these functions are used with the WhereFilter, the related
+    entities are in fact included entities (entities which are also present in the input
+    of an include filter in the same request). However, when these functions are used
+    with the DistinctFilter, they are related entities, not included entities as there's
+    no requirement for the entity names to also be present in an include filter (this is
+    to match the ICAT backend)
+    """
+
+    def __init__(self):
+        self.related_field = None
+        self.related_related_field = None
+
+    def _extract_filter_fields(self, field):
+        """
+        Extract the related fields names and put them into separate variables
+
+        :param field: ICAT field names, separated by dots
+        :type field: :class:`str`
+        :raises ValueError: If the maximum related/included depth is exceeded
+        """
+
+        fields = field.split(".")
+        related_depth = len(fields)
+
+        log.debug("Fields: %s, Related Depth: %d", fields, related_depth)
+
+        if related_depth == 1:
+            self.field = fields[0]
+        elif related_depth == 2:
+            self.field = fields[0]
+            self.related_field = fields[1]
+        elif related_depth == 3:
+            self.field = fields[0]
+            self.related_field = fields[1]
+            self.related_related_field = fields[2]
+        else:
+            raise ValueError(f"Maximum related depth exceeded. {field}'s depth > 3")
+
+        # TODO - Remove if not needed
+        # return (field, related_field, related_related_field)
+
+    def _add_query_join(self, query):
+        """
+        Fetches the appropriate entity model based on the contents of `self.field` and
+        adds any required JOINs to the query if any related fields have been used in the
+        filter
+
+        :param query: The query to have filters applied to
+        :type query: :class:`datagateway_api.common.database.helpers.[QUERY]`
+        :return: Entity model of the field (usually the field relating to the endpoint
+            the request is coming from)
+        """
+        try:
+            field = getattr(query.table, self.field)
+        except AttributeError:
+            raise FilterError(
+                f"Unknown attribute {self.field} on table {query.table.__name__}",
+            )
+
+        if self.related_related_field:
+            included_table = getattr(models, self.field)
+            included_included_table = getattr(models, self.related_field)
+            query.base_query = query.base_query.join(included_table).join(
+                included_included_table,
+            )
+            field = getattr(included_included_table, self.related_related_field)
+        elif self.related_field:
+            included_table = get_entity_object_from_name(self.field)
+            query.base_query = query.base_query.join(included_table)
+            field = getattr(included_table, self.related_field)
+
+        return field
+
+
 class DatabaseWhereFilter(WhereFilter):
     def __init__(self, field, value, operation):
         super().__init__(field, value, operation)
@@ -95,17 +174,24 @@ class DatabaseWhereFilter(WhereFilter):
             )
 
 
-class DatabaseDistinctFieldFilter(DistinctFieldFilter):
+class DatabaseDistinctFieldFilter(DistinctFieldFilter, DatabaseFilterUtilities):
     def __init__(self, fields):
-        super().__init__(fields)
+        # TODO - what's the Pythonic solution here?
+        # super().__init__(fields)
+        DistinctFieldFilter.__init__(self, fields)
+        DatabaseFilterUtilities.__init__(self)
 
     def apply_filter(self, query):
         query.is_distinct_fields_query = True
         try:
-            self.fields = [getattr(query.table, field) for field in self.fields]
+            distinct_fields = []
+            for field_name in self.fields:
+                self._extract_filter_fields(field_name)
+                field = self._add_query_join(query)
+                distinct_fields.append(field)
         except AttributeError:
             raise FilterError("Bad field requested")
-        query.base_query = query.session.query(*self.fields).distinct()
+        query.base_query = query.session.query(*distinct_fields).distinct()
 
 
 class DatabaseOrderFilter(OrderFilter):
