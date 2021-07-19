1b17a29925eaf7312a150c82d4232fec4a4709f8
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-07-19 15:02:58 +0000

    #238: Add test cases for one-to-many related ordering

--- test/icat/filters/test_order_filter.py
+++ test/icat/filters/test_order_filter.py
@@ -30,6 +30,50 @@ class TestICATOrderFilter:
 
         filter_handler.clear_python_icat_order_filters()
 
+    def test_join_specs_added(self, icat_query):
+        pid_filter = PythonICATOrderFilter("studyInvestigations.study.pid", "ASC")
+        name_filter = PythonICATOrderFilter(
+            "investigationInstruments.instrument.name", "DESC",
+        )
+
+        filter_handler = FilterOrderHandler()
+        filter_handler.add_filters([pid_filter, name_filter])
+        filter_handler.apply_filters(icat_query)
+
+        assert PythonICATOrderFilter.join_specs == {
+            "studyInvestigations": "LEFT JOIN",
+            "studyInvestigations.study": "LEFT JOIN",
+            "investigationInstruments": "LEFT JOIN",
+            "investigationInstruments.instrument": "LEFT JOIN",
+        }
+
+        filter_handler.clear_python_icat_order_filters()
+
+    def test_valid_one_many_related_ordering(self, icat_query):
+        pid_filter = PythonICATOrderFilter("studyInvestigations.study.pid", "DESC")
+        filter_handler = FilterOrderHandler()
+        filter_handler.add_filter(pid_filter)
+        filter_handler.apply_filters(icat_query)
+
+        assert icat_query.join_specs == {
+            "studyInvestigations": "LEFT JOIN",
+            "studyInvestigations.study": "LEFT JOIN",
+        }
+
+        filter_handler.clear_python_icat_order_filters()
+
+    def test_invalid_one_many_related_ordering(self, icat_query):
+        pid_filter = PythonICATOrderFilter("studyInvestigations.study.pid", "DESC")
+        filter_handler = FilterOrderHandler()
+        filter_handler.add_filter(pid_filter)
+
+        PythonICATOrderFilter.join_specs["testEntities"] = "LEFT JOIN"
+
+        with pytest.raises(FilterError):
+            filter_handler.apply_filters(icat_query)
+
+        filter_handler.clear_python_icat_order_filters()
+
     def test_filter_applied_to_query(self, icat_query):
         test_filter = PythonICATOrderFilter("id", "DESC")
 
