f6e8ebd5d887d12e8b58dfe55ee3226331ea34f5
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-08-13 07:33:22 +0000

    #250: Add test to mock invalid backend on query filter getter

--- test/test_query_filter.py
+++ test/test_query_filter.py
@@ -1,4 +1,10 @@
+from unittest.mock import patch
+
+import pytest
+
+from datagateway_api.common.exceptions import ApiError
 from datagateway_api.common.filters import QueryFilter
+from datagateway_api.common.query_filter_factory import QueryFilterFactory
 
 
 class TestQueryFilter:
@@ -16,3 +22,11 @@ class TestQueryFilter:
 
         assert qf.precedence is None
         assert qf.apply_filter(apply_filter) is None
+
+    def test_invalid_query_filter_getter(self):
+        with patch(
+            "datagateway_api.common.config.config.get_config_value",
+            return_value="invalid_backend",
+        ):
+            with pytest.raises(ApiError):
+                QueryFilterFactory.get_query_filter({"order": "id DESC"})
