1049a5f82feef3f5909440e4e928a43f969c4219
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-11-24 17:19:42 +0000

    add helper functions for each endpoint type for search API #258

--- datagateway_api/src/search_api/helpers.py
+++ datagateway_api/src/search_api/helpers.py
@@ -1,3 +1,42 @@
-"""
-Code to store helper functions to deal with the endpoints, like for DataGateway API
-"""
+import logging
+
+from datagateway_api.src.search_api.session_handler import (
+    SessionHandler,
+    client_manager,
+)
+
+
+log = logging.getLogger()
+
+
+# TODO - Make filters mandatory, if no filters are in a request an empty list will be
+# given to these functions
+@client_manager
+def get_search(entity_name, filters=None):
+    # TODO - Remove this debug logging when implementing the endpoints, this is just to
+    # show the client handling works
+    log.debug(
+        "Client: %s, Session ID: %s",
+        SessionHandler.client,
+        SessionHandler.client.sessionId,
+    )
+
+
+@client_manager
+def get_with_id(entity_name, id, filters=None):
+    pass
+
+
+@client_manager
+def get_count(entity_name, filters=None):
+    pass
+
+
+@client_manager
+def get_files(entity_name, filters=None):
+    pass
+
+
+@client_manager
+def get_files_count(entity_name, id, filters=None):
+    pass
