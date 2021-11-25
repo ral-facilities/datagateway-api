b7dea7378be9fee6c0323b7ec8b9608ff87996a3
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-11-25 16:49:11 +0000

    test: add tests for search API session/client handling #258

--- test/datagateway_api/icat/test_icat_client.py
+++ test/datagateway_api/icat/test_icat_client.py
@@ -1,5 +1,10 @@
-from icat.client import Client
+import json
+from unittest.mock import mock_open, patch
 
+from icat.client import Client
+import pytest
+
+from datagateway_api.src.common.config import APIConfig
 from datagateway_api.src.datagateway_api.icat.icat_client_pool import ICATClient
 
 
@@ -9,6 +14,47 @@ class TestICATClient:
         assert isinstance(test_icat_client, Client)
 
         assert not test_icat_client.autoLogout
+
+    @pytest.mark.parametrize(
+        "client_use, expected_url, expected_check_cert",
+        [
+            pytest.param(
+                "datagateway_api",
+                "https://localhost:8181/ICATService/ICAT?wsdl",
+                False,
+                id="DataGateway API Usage",
+            ),
+            pytest.param(
+                "search_api",
+                "https://localhost.testdomain:8181/ICATService/ICAT?wsdl",
+                True,
+                id="Search API Usage",
+            ),
+        ],
+    )
+    def test_client_use(
+        self, test_config_data, client_use, expected_url, expected_check_cert,
+    ):
+        with patch("builtins.open", mock_open(read_data=json.dumps(test_config_data))):
+            api_config = APIConfig.load("test/path")
+
+            class MockClient:
+                def __init__(url, checkCert=True):  # noqa
+                    print(f"URL: {url}, Cert: {checkCert}")
+                    # Would've preferred to assign these values to self but this didn't
+                    # seem to be possible
+                    Client.url = f"{url}/ICATService/ICAT?wsdl"
+                    Client.checkCert = checkCert
+
+            with patch(
+                "datagateway_api.src.common.config.Config.config", api_config,
+            ):
+                with patch(
+                    "icat.client.Client.__init__", side_effect=MockClient.__init__,
+                ):
+                    test_icat_client = ICATClient(client_use)
+                    assert test_icat_client.url == expected_url
+                    assert test_icat_client.checkCert == expected_check_cert
 
     def test_clean_up(self):
         test_icat_client = ICATClient()
