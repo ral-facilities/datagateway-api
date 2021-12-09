8479d23c332fb95aa88fa450c2eb6792c78fce1a
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-12-09 11:14:57 +0000

    add basic implementation of `SearchAPIQuery` #265

---
+++ datagateway_api/src/search_api/query.py
@@ -0,0 +1,14 @@
+from datagateway_api.src.datagateway_api.icat.query import ICATQuery
+from datagateway_api.src.search_api.panosc_mappings import mappings
+from datagateway_api.src.search_api.session_handler import SessionHandler
+
+
+# TODO - Not sure if this should inherit from `ICATQuery`?
+class SearchAPIQuery:
+    def __init__(self, panosc_entity_name):
+        self.panosc_entity_name = panosc_entity_name
+        self.icat_entity_name = mappings.mappings[panosc_entity_name][
+            "base_icat_entity",
+        ]
+
+        self.query = ICATQuery(SessionHandler.client, self.icat_entity_name)
