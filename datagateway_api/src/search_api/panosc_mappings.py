cba6482519e112bba2ae21ff320728a5a631b184
Matthew Richards <matthew.richards@stfc.ac.uk>
2021-12-20 17:50:52 +0000

    style: remove unneeded code #260

--- datagateway_api/src/search_api/panosc_mappings.py
+++ datagateway_api/src/search_api/panosc_mappings.py
@@ -8,6 +8,7 @@ class PaNOSCMappings:
     def __init__(
         self, path=Path(__file__).parent.parent.parent / "search_api_mapping.json",
     ):
+        """Load contents of `search_api_mapping.json` into this class"""
         try:
             with open(path, encoding="utf-8") as target:
                 self.mappings = json.load(target)
@@ -42,17 +43,6 @@ class PaNOSCMappings:
             )
 
         return panosc_related_entity_name
-
-
-# TODO - don't think I'll need this
-def load_mappings(path=Path(__file__).parent.parent.parent / "search_api_mapping.json"):
-    try:
-        with open(path, encoding="utf-8") as target:
-            data = json.load(target)
-    except IOError as e:
-        raise SearchAPIError(e)
-
-    return data
 
 
 mappings = PaNOSCMappings()
