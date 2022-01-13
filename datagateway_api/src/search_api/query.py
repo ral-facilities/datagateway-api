34fa7d9896985da0bc3c1686cf40a28109d48b1f
Matthew Richards <matthew.richards@stfc.ac.uk>
2022-01-13 17:03:23 +0000

    refactor: Pass kwargs into query class #268
    - This allows the `COUNT` keyword to be specified during a search API endpoint

--- datagateway_api/src/search_api/query.py
+++ datagateway_api/src/search_api/query.py
@@ -4,13 +4,15 @@ from datagateway_api.src.search_api.session_handler import SessionHandler
 
 
 class SearchAPIQuery:
-    def __init__(self, panosc_entity_name):
+    def __init__(self, panosc_entity_name, **kwargs):
         self.panosc_entity_name = panosc_entity_name
         self.icat_entity_name = mappings.mappings[panosc_entity_name][
             "base_icat_entity"
         ]
 
-        self.icat_query = ICATQuery(SessionHandler.client, self.icat_entity_name)
+        self.icat_query = ICATQuery(
+            SessionHandler.client, self.icat_entity_name, **kwargs,
+        )
 
     def __repr__(self):
         return (
