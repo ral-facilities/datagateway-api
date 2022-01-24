b8178a2b903475b0f7e6db039fec1e8d52cdf838
Viktor Bozhinov <viktor_bozinov1@hotmail.com>
2022-01-24 17:51:40 +0000

    test: unit test method for getting non related field names of PaNOSC entity #265

--- test/search_api/test_panosc_mappings.py
+++ test/search_api/test_panosc_mappings.py
@@ -1,6 +1,6 @@
 import pytest
 
-from datagateway_api.src.common.exceptions import SearchAPIError
+from datagateway_api.src.common.exceptions import FilterError, SearchAPIError
 from datagateway_api.src.search_api.panosc_mappings import PaNOSCMappings
 
 
@@ -36,3 +36,63 @@ class TestPaNOSCMappings:
             test_panosc_mappings.get_panosc_related_entity_name(
                 "UnknownField", "unknownField",
             )
+
+    @pytest.mark.parametrize(
+        "test_panosc_entity_name, expected_non_related_field_names",
+        [
+            pytest.param(
+                "Affiliation",
+                ["id", "name", "address", "city", "country"],
+                id="Affiliation",
+            ),
+            pytest.param(
+                "Dataset",
+                ["pid", "title", "isPublic", "creationDate", "size"],
+                id="Dataset",
+            ),
+            pytest.param(
+                "Document",
+                [
+                    "pid",
+                    "isPublic",
+                    "type",
+                    "title",
+                    "summary",
+                    "doi",
+                    "startDate",
+                    "endDate",
+                    "releaseDate",
+                    "license",
+                    "keywords",
+                ],
+                id="Document",
+            ),
+            pytest.param("File", ["id", "name", "path", "size"], id="File"),
+            pytest.param("Instrument", ["pid", "name", "facility"], id="Instrument"),
+            pytest.param("Member", ["id", "role"], id="Member"),
+            pytest.param("Parameter", ["id", "name", "value", "unit"], id="Parameter"),
+            pytest.param(
+                "Person",
+                ["id", "fullName", "orcid", "researcherId", "firstName", "lastName"],
+                id="Person",
+            ),
+            pytest.param("Sample", ["name", "pid", "description"], id="Sample"),
+            pytest.param("Technique", ["pid", "name"], id="Technique"),
+        ],
+    )
+    def test_valid_get_panosc_non_related_field_names(
+        self,
+        test_panosc_mappings,
+        test_panosc_entity_name,
+        expected_non_related_field_names,
+    ):
+        non_related_field_names = test_panosc_mappings.get_panosc_non_related_field_names(  # noqa: B950
+            test_panosc_entity_name,
+        )
+        assert non_related_field_names == expected_non_related_field_names
+
+    def test_invalid_get_panosc_non_related_field_names(
+        self, test_panosc_mappings,
+    ):
+        with pytest.raises(FilterError):
+            test_panosc_mappings.get_panosc_non_related_field_names("UnknownEntity")
