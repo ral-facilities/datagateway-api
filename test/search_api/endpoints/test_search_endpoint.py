f1539db866400caa8cee4080b4c0e4e906f8af22
Matthew Richards <matthew.richards@stfc.ac.uk>
2022-02-02 15:32:57 +0000

    test: fix tests after fixing bug #268

--- test/search_api/endpoints/test_search_endpoint.py
+++ test/search_api/endpoints/test_search_endpoint.py
@@ -221,122 +221,18 @@ class TestSearchAPISearchEndpoint:
                 '{"include": [{"relation": "documents"}, {"relation": "techniques"},'
                 ' {"relation": "instrument"}, {"relation": "files"},'
                 ' {"relation": "parameters"}, {"relation": "samples"}], "limit": 1}',
-                [
-                    {
-                        "createId": "user",
-                        "startDate": "2000-10-13 00:00:00+00:00",
-                        "doi": "0-8401-1070-7",
-                        "modTime": "2006-08-24 01:28:06+00:00",
-                        "createTime": "2013-04-01 10:56:52+00:00",
-                        "location": "/subject/break.jpeg",
-                        "endDate": "2000-02-10 00:00:00+00:00",
-                        "complete": True,
-                        "modId": "user",
-                        "documents": [{}],
-                        "description": "Beat professional blue clear style have. Light"
-                        " final summer. Or hour color maybe word side much team."
-                        "\nMessage weight official learn especially nature. Himself"
-                        " tax west.",
-                        "name": "DATASET 2",
-                        "id": 2,
-                    },
-                ],
+                [{}],
                 id="Search datasets including all possible related entities",
-                # Skipped because of incorrect document JSON format and ICAT 5 mapping
-                # on techniques and instrument
+                # Skipped because ICAT 5 mapping on techniques and instrument
                 marks=pytest.mark.skip,
             ),
             pytest.param(
                 "documents",
                 '{"include": [{"relation": "datasets"}, {"relation": "members"},'
                 ' {"relation": "parameters"}], "limit": 1}',
-                [
-                    {
-                        "datasets": [
-                            {
-                                "createId": "user",
-                                "startDate": "2000-05-07 00:00:00+00:00",
-                                "doi": "0-449-78690-0",
-                                "modTime": "2005-04-30 19:41:49+00:00",
-                                "createTime": "2002-11-27 06:20:36+00:00",
-                                "location": "/international/subject.tiff",
-                                "endDate": "2000-07-05 00:00:00+00:00",
-                                "complete": True,
-                                "modId": "user",
-                                "description": "Many last prepare small. Maintain throw"
-                                " hope parent.\nEntire soon option bill fish against"
-                                " power."
-                                "\nRather why rise month shake voice.",
-                                "parameters": [],
-                                "name": "DATASET 1",
-                                "id": 1,
-                            },
-                            {
-                                "createId": "user",
-                                "startDate": "2060-01-07 00:00:00+00:00",
-                                "doi": "0-353-84629-5",
-                                "modTime": "2002-09-30 13:03:32+00:00",
-                                "createTime": "2006-11-21 17:10:42+00:00",
-                                "location": "/gun/special.jpeg",
-                                "endDate": "2060-01-17 00:00:00+00:00",
-                                "complete": True,
-                                "modId": "user",
-                                "description": "Single many hope organization reach"
-                                " process I. Health hit total federal describe. Bill"
-                                " firm rate democratic outside.\nLate while our either"
-                                " worry.",
-                                "parameters": [],
-                                "name": "DATASET 241",
-                                "id": 241,
-                            },
-                        ],
-                        "createId": "user",
-                        "doi": "0-449-78690-0",
-                        "startDate": "2000-04-03 00:00:00+00:00",
-                        "modTime": "2005-04-30 19:41:49+00:00",
-                        "createTime": "2002-11-27 06:20:36+00:00",
-                        "members": [
-                            {
-                                "createId": "user",
-                                "modId": "user",
-                                "modTime": "2005-04-30 19:41:49+00:00",
-                                "role": "CI",
-                                "createTime": "2002-11-27 06:20:36+00:00",
-                                "id": 1,
-                            },
-                        ],
-                        "endDate": "2000-07-09 00:00:00+00:00",
-                        "modId": "user",
-                        "title": "Including spend increase ability music skill former."
-                        " Agreement director concern once technology sometimes someone"
-                        " staff.\nSuccess pull bar. Laugh senior example.",
-                        "summary": "Season identify professor happen third. Beat"
-                        " professional blue clear style have. Light final summer.",
-                        "visitId": "42",
-                        "parameters": [
-                            {
-                                "dateTimeValue": "2000-05-07 00:00:00+00:00",
-                                "rangeBottom": 48.0,
-                                "createId": "user",
-                                "numericValue": 127265.0,
-                                "modTime": "2005-04-30 19:41:49+00:00",
-                                "createTime": "2002-11-27 06:20:36+00:00",
-                                "id": 1,
-                                "stringValue": "international1",
-                                "modId": "user",
-                                "rangeTop": 101.0,
-                                "error": 31472.0,
-                            },
-                        ],
-                        "name": "INVESTIGATION 1",
-                        "releaseDate": "2000-07-05 00:00:00+00:00",
-                        "id": 1,
-                    },
-                ],
+                [{}],
                 id="Search documents including all possible related entities",
-                # Skipped because of incorrect members JSON naming
-                # (investigationUsers key used instead). It's in ICAT fields too,
-                # not converted into PaNOSC format
+                # TODO - issue with parameters include
                 marks=pytest.mark.skip,
             ),
             pytest.param(
