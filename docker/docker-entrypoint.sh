0e948f7c2291efc937c117c49f4e7cc6649b9d2a
Alan Kyffin <alan.kyffin@stfc.ac.uk>
2023-02-22 15:35:56 +0000

    log_location value should not be quoted

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # Set values in config.yaml from environment variables
 sed -i "s|icat_url: \".*\"|icat_url: \"$ICAT_URL\"|" /datagateway-api-run/config.yaml
 sed -i "s|icat_check_cert: \".*\"|icat_check_cert: \"$ICAT_CHECK_CERT\"|" /datagateway-api-run/config.yaml
-sed -i "s|log_location: \".*\"|log_location: \"$LOG_LOCATION\"|" /datagateway-api-run/config.yaml
+sed -i "s|log_location: .*|log_location: $LOG_LOCATION|" /datagateway-api-run/config.yaml
 
 # Run the CMD instruction
 exec "$@"
