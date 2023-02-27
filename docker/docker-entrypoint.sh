14434c9c0cd28c5c14864693a4ba7eaa7db78149
Alan Kyffin <alan.kyffin@stfc.ac.uk>
2023-02-27 10:04:32 +0000

    Use a temp file instead of sed -i in entrypoint script

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/sh -eu
+
+# Use a tempfile instead of sed -i so that only the file, not the directory needs to be writable
+TEMPFILE="$(mktemp)"
 
 # Set values in config.yaml from environment variables
-sed -i "s|icat_url: \".*\"|icat_url: \"$ICAT_URL\"|" /datagateway-api-run/config.yaml
-sed -i "s|icat_check_cert: \".*\"|icat_check_cert: \"$ICAT_CHECK_CERT\"|" /datagateway-api-run/config.yaml
-sed -i "s|log_location: .*|log_location: $LOG_LOCATION|" /datagateway-api-run/config.yaml
+# No quotes for icat_check_cert because it's boolean
+sed -e "s|icat_url: \".*\"|icat_url: \"$ICAT_URL\"|" \
+    -e "s|icat_check_cert: .*|icat_check_cert: $ICAT_CHECK_CERT|" \
+    -e "s|log_location: \".*\"|log_location: \"$LOG_LOCATION\"|" \
+    datagateway_api/config.yaml > "$TEMPFILE"
+
+cat "$TEMPFILE" > datagateway_api/config.yaml
+rm "$TEMPFILE"
 
 # Run the CMD instruction
 exec "$@"
