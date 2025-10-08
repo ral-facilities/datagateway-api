24c1b6e142ee74f9939adf9c061b8f3fc48b685f
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2025-10-08 13:45:03 +0100

    remove install with constraints #517

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,3 @@
-import os
-import tempfile
-
 import nox
 
 # Separating Black away from the rest of the sessions
@@ -8,85 +5,31 @@ nox.options.sessions = "lint", "safety", "tests"
 code_locations = "datagateway_api", "test", "util", "noxfile.py"
 
 
-def install_with_constraints(session, *args, **kwargs):
-    # Auto file deletion is turned off to prevent a PermissionError experienced on
-    # Windows
-    with tempfile.NamedTemporaryFile(delete=False) as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
-
-        try:
-            # Due to delete=False, the file must be deleted manually
-            requirements.close()
-            os.unlink(requirements.name)
-        except IOError:
-            session.log("Error: The temporary requirements file could not be closed")
-
-
 @nox.session(reuse_venv=True)
 def black(session):
     args = session.posargs or code_locations
 
-    install_with_constraints(session, "black")
+    session.run("poetry", "install", "--only=dev", external=True)
     session.run("black", *args, external=True)
 
 
 @nox.session(reuse_venv=True)
 def lint(session):
     args = session.posargs or code_locations
-    install_with_constraints(
-        session,
-        "flake8",
-        "flake8-bandit",
-        "flake8-black",
-        "flake8-broken-line",
-        "flake8-bugbear",
-        "flake8-builtins",
-        "flake8-commas",
-        "flake8-comprehensions",
-        "flake8-import-order",
-        "flake8-logging-format",
-        "pep8-naming",
-    )
+    session.run("poetry", "install", "--only=dev", external=True)
     session.run("flake8", *args)
 
 
 @nox.session(reuse_venv=True)
 def safety(session):
-    install_with_constraints(session, "safety")
-    with tempfile.NamedTemporaryFile(delete=False) as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        # Ignore vulnerabilities as the patched versions of dependencies that they
-        # relate to don't support Python 3.6 which is still required for production
-        session.run(
-            "safety",
-            "check",
-            f"--file={requirements.name}",
-            "--full-report",
-        )
-
-        try:
-            # Due to delete=False, the file must be deleted manually
-            requirements.close()
-            os.unlink(requirements.name)
-        except IOError:
-            session.log("Error: The temporary requirements file could not be closed")
+    session.install("safety")
+    # Ignore vulnerabilities as the patched versions of dependencies that they
+    # relate to don't support Python 3.6 which is still required for production
+    session.run(
+        "safety",
+        "check",
+        "--full-report",
+    )
 
 
 @nox.session(python=["3.10"], reuse_venv=True)
